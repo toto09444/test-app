aa27982a78e7e502e6c34f6c8641a9304ec380eb
Donmark2k <chukwuemekavp@gmail.com>
2023-09-05 13:45:00 +0100

    Design the status relationship with other model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Status;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Listing extends Model
 {
@@ -26,5 +27,10 @@ class Listing extends Model
     // Relationship To User
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // Relationship With Status
+    public function status() {
+        return $this->hasMany(Status::class, 'listing_id');
     }
 }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Listing;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Status extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'listing_id', 'applied',];
+
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'applied_on' => 'datetime',
+    ];
+
+    // Relationship To User
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // Relationship To User
+    public function listing() {
+        return $this->belongsTo(Listing::class, 'listing_id');
+    }
 }
